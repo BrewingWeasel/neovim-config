d9d2815dc996f84a6e6909326745d4512e1c3f0e
Finn Brewer <weeaseled@gmail.com>
2024-03-14 21:52:21 -0700

    feat: add keybinds to control editor

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,3 +2,17 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "[y]ank to the system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "[p]aste from the system clipboard" })
+
+vim.keymap.set("n", "<leader>nw", function()
+	vim.cmd("set wrap!")
+end, {
+	desc = "[n]eovim: toggle [w]rap",
+})
+
+vim.keymap.set("n", "<leader>nl", function()
+	vim.cmd("Lazy")
+end, { desc = "[n]eovim: open [l]azy" })
+
+vim.keymap.set("n", "<leader>nm", function()
+	vim.cmd("Mason")
+end, { desc = "[n]eovim: open [m]ason" })
